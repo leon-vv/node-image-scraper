d5479da18f5cecab148f2ddab4e73dd453905bad
Léon van Velzen <leon-vv@hotmail.com>
2013-06-26 20:44:46 +0200

    Initialization

---
+++ index.js
@@ -0,0 +1,136 @@
+var fs = require("fs");
+var http = require("http");
+var https = require("https");
+var path = require("path");
+var url = require("url");
+var events = require("events").EventEmitter;
+var util = require("util");
+
+var cheerio = require("cheerio");
+
+// The "Image" class.
+function Image(image, address){
+
+	var at = this.attributes = image.attribs;
+
+	this.name = path.basename(at.src, path.extname(at.src));
+	this.saveTo = path.dirname(require.main.filename) + "/";
+	this.extension = path.extname(at.src);
+	this.address = url.resolve(address, at.src);
+	this.fromAddress = address;
+
+	console.log(this.saveTo);
+}
+
+Image.prototype.save = function(){
+
+	var parsedUrl = url.parse(this.address);
+
+	// Make a reference to the current instance.
+	var ref = this;
+
+	// Support HTTPS.
+	if(parsedUrl.protocol == "https:") var protocol = https;
+	else var protocol = http;
+
+	var request = protocol.request(this.address, function(response){
+
+		if(response.statusCode != 200){
+		
+			console.error("Image scraper(3): image couldn't be found.");
+		}
+		else{
+
+			var imageFile = fs.createWriteStream(path.normalize(ref.saveTo + ref.name + ref.extension));
+
+			imageFile.on("error", function(e){
+
+				console.error("Image scraper(4): error while loading image: " + e + ".");
+			});
+
+			response.on("data", function(data){
+
+				imageFile.write(data);
+			});
+
+			response.on("end", function(){
+
+				imageFile.end();
+			});
+		}
+	});
+
+	request.end();
+	request.on("error", function(e){
+
+		console.error(e);
+	});
+};
+
+function Scraper(address){
+
+	events.call(this);
+	this.address = address;
+	this.setMaxListeners(1);
+};
+
+// Inherit the methods of "events".
+util.inherits(Scraper, events);
+
+Scraper.prototype.scrape = function(callback){
+
+	if(typeof(callback) == "function"){
+
+		this.on("image", callback);
+	}
+
+	var parsedUrl = url.parse(this.address);
+
+	// Make a reference to the current instance.
+	var ref = this;
+
+	// Support HTTPS.
+	if(parsedUrl.protocol == "https:") var protocol = https;
+	else var protocol = http;
+
+	var request = protocol.request(this.address, function(response){
+
+		if(response.statusCode != 200){
+		
+			console.error("Image scraper(1): web page couldn't be found.");
+		}
+		else{
+
+			response.setEncoding("utf8");
+
+			var previous = "",
+			current;
+
+			response.on("data", function(data){
+
+				var current = previous + data;
+
+				current.replace(/<img.*?\/>/ig, function(m){
+
+					var image = new Image(cheerio.load(m)("img")[0], ref.address);
+
+					ref.emit("image", image);
+				});
+
+				var previous = data;
+			});
+
+			response.on("end", function(){
+
+			});
+		}
+	});
+	request.end();
+
+	request.on("error", function(e){
+
+		console.error("Image scraper(2): error while loading web page: " + e + ".");
+	});
+};
+
+module.exports = Scraper;
